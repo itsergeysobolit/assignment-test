5b534465cfafa82cf97091e2a34ebb7b9cb809d1
Serhii Sobol <itsergeysobol@gmail.com>
2021-04-27 22:14:34 +0300

    update js file & add css & change html

--- cors-anywhere.js
+++ cors-anywhere.js
@@ -3,12 +3,12 @@ var host = process.env.HOST || '127.0.0.1';
 // Listen on a specific port via the PORT environment variable
 var port = process.env.PORT || 8089;
 
-var express = require('express')
-var cors = require('cors')
-var cors_proxy = require('cors-anywhere');
+var express = require('express') //framework on node.js https://expressjs.com/
+var cors = require('cors') //CORS is a node.js package for providing a Connect/Express middleware that can be used to enable CORS with various options. https://www.npmjs.com/package/cors
+var cors_proxy = require('cors-anywhere'); //CORS Anywhere is a NodeJS proxy which adds CORS headers to the proxied request. https://www.npmjs.com/package/cors-anywhere
 var app = express()
 
-app.use(cors())
+app.use(cors()) // chect stackoverflow
 
 cors_proxy.createServer({
     originWhitelist: [], // Allow all origins
